bf15da4dcce486711c592fb13b7f000ebad4860c
Collins Kiprono <one.kcollin@gmail.com>
2022-08-19 11:05:50 +0300

    add: setup autoloaders & helpers

--- functions.php
+++ functions.php
@@ -5,9 +5,18 @@
  * @package Aquila
  */
 
+ if(!defined('AQUILA_DIR_PATH')){
+     define('AQUILA_DIR_PATH', untrailingslashit( get_template_directory() ));
+ }
+
+
+ require_once AQUILA_DIR_PATH.'/inc/helpers/autoloader.php';
+
  /**
  * Proper way to enqueue scripts and styles.
  */
+
+
 
 function aquila_scripts() {
     /** Good practice to register style first the enqueue later */
